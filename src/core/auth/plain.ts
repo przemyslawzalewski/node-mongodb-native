deb533f611f70427a6f513237a78d31a1718f033
Dan Aprahamian <dan.aprahamian@gmail.com>
2019-07-15 15:41:20 -0400

    chore(tsc): convert lib/core/auth/plain

---
+++ src/core/auth/plain.ts
@@ -0,0 +1,44 @@
+'use strict';
+
+import { retrieveBSON } from '../connection/utils';
+import { AuthProvider } from './auth_provider';
+import { RunCommandOnConnection } from '../../../interfaces/run_command_on_connection';
+import { ConnectionInterface } from '../../../interfaces/connection';
+import { MongoCredentials } from './mongo_credentials';
+
+// TODO: can we get the Binary type from this.bson instead?
+const BSON = retrieveBSON();
+const Binary = BSON.Binary;
+
+/**
+ * Creates a new Plain authentication mechanism
+ *
+ * @extends AuthProvider
+ */
+class Plain extends AuthProvider {
+  /**
+   * Implementation of authentication for a single connection
+   * @override
+   */
+  _authenticateSingleConnection(
+    sendAuthCommand: RunCommandOnConnection,
+    connection: ConnectionInterface, 
+    credentials: MongoCredentials,
+    callback: (err: Error|null|undefined, result: any) => void
+  ) {
+    const username = credentials.username;
+    const password = credentials.password;
+    // TS-TODO: come back to this
+    const payload = new Binary(`\x00${username}\x00${password}` as any);
+    const command = {
+      saslStart: 1,
+      mechanism: 'PLAIN',
+      payload: payload,
+      autoAuthorize: 1
+    };
+
+    sendAuthCommand(connection, '$external.$cmd', command, callback);
+  }
+}
+
+module.exports = Plain;
